88316a9d49e764209a6607cd629462f9988be0d4
Yahya Yağmur <pprojetestleri@gmail.com>
2024-07-11 22:10:57 +0300

    added favorites

--- src/main/java/Base/BaseLibrary.java
+++ src/main/java/Base/BaseLibrary.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class BaseLibrary extends Data {
     public static WebDriver driver;
     public void sleep(int time) throws InterruptedException {
@@ -16,6 +19,11 @@ public class BaseLibrary extends Data {
         return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 
+    public void switchTab()
+    {
+        ArrayList<String> list = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(list.get(1));
+    }
 
 
 }
--- src/main/java/Pages/MainPage.java
+++ src/main/java/Pages/MainPage.java
@@ -3,6 +3,7 @@ package Pages;
 import Base.BaseLibrary;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 public class MainPage extends BaseLibrary {
@@ -12,6 +13,21 @@ public class MainPage extends BaseLibrary {
         WebElement element = driver.findElement(By.id("onetrust-accept-btn-handler"));
         if (element.isDisplayed())
             driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+        return this;
+    }
+
+    @Step("Arama İşlemi Sağlanır")
+    public MainPage aramaDoldur(String text) {
+        WebElement element = driver.findElement(By.cssSelector("[data-testid='suggestion']"));
+        element.sendKeys(text, Keys.ENTER);
+        return this;
+    }
+
+    @Step("Favoriler Alanına Tıklanır ")
+    public MainPage clickFavorites()
+    {
+        WebElement element = driver.findElement(By.cssSelector("[class=\"account-nav-item account-favorites\"]"));
+        element.click();
         return this;
     }
 }
